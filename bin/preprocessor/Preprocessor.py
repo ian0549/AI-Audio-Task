89caf67fe7a04bd018f747cf3c5c38668948294e
ian akoto <iancecilakoto@gmail.com>
2021-02-18 12:55:18 +0000

    test function and pipeline implemented

--- bin/preprocessor/Preprocessor.py
+++ bin/preprocessor/Preprocessor.py
@@ -1,49 +1,67 @@
 import os
 
 import numpy as np
-import librosa 
+import librosa
 import pickle
+import functools
+from pathlib import Path
+
 
 
 
 class Pipeline:
-
- 	""" ETL Pipeline class that does the following:
+     """ ETL Pipeline class that does the following:
 
         1. Load audio files from a directory
         2. Extract features for each audio file
         3. Save features as numpy arrays in another directory.
-        
-	
-	Attributes:
+
+
+     Attributes:
         dataset_path (string) representing the path for the dataset
         output_path (string)  representing the path to save all the mfccs or melspect features
-		feature_type (string) representing the type of features to extract from the audio dataset. Either MFCCs or Mel-Spectrograms
-		mel_bands (int) representing the range of frequencies that are relevant
-		n_mfcc (int) 
-        n_fft (int) this representing the interval of the number of sample to consider for the fourier transform 
+        feature_type (string) representing the type of features to extract from the audio dataset. Either MFCCs or Mel-Spectrograms
+        mel_bands (int) representing the range of frequencies that are relevant
+        n_mfcc (int)
+        n_fft (int) this representing the interval of the number of sample to consider for the fourier transform
         hop_length (int)
         num_segments (int)
+        
 
 
 
-	"""
+     """
 
-
-
-    def __init__(self, 
+     def __init__(self,
                 dataset_path,
                 output_path,
-                feature_type,
+                sample_rate=22050,
                 mel_bands=10,
                 n_mfcc=13,
-                hop_length=512,
-                num_segments=5):
-        
-        # initialize attritubes
+                n_fft=2048,
+                hop_length=512):
+                
 
 
-    def _loader(self):
+
+            # initialize attritubes
+
+            self.dataset_path=dataset_path
+            self.output_path=output_path
+            self.mel_bands=mel_bands
+            self.n_mfcc=n_mfcc
+            self.n_fft=n_fft
+            self.hop_length=hop_length
+
+            self.sample_rate = sample_rate
+
+     def __getitem__(self,key):
+        return getattr(self,key)
+          
+
+
+     @functools.lru_cache(maxsize = None) 
+     def loader(self):
 
         """Function to load audio files from dataset using librosa.
             
@@ -54,24 +72,50 @@ class Pipeline:
                 signal (float): list of signals for each audio file
         
         """
-        
+        audio_signals=[]
+        path, dirs, files = next(os.walk(self.dataset_path))
+
+        for f in files:
+
+            file_path = os.path.join(path, f)
+
+            signal, _ = librosa.load(file_path, sr=self.sample_rate)
+            audio_signals.append(signal)
+
+
+        return  audio_signals   
 
 
 
-    def _extract_mfccs(self, loaded_signals):
+
+     def extract_mfccs(self, loaded_signals):
         
         """Function to extract mfccs from loaded audio files using librosa.
             
             Args: 
-                loaded_signals (float): list of signals for each audio file
+                loaded_signals (array list): list of signals for each audio file
             
             Returns: 
-                features (numpy arrays): list of mfccs extracted from audio signal
+                features (arrays list): list of mfccs extracted from audio signal
         
         """
 
+        featrue_list=[]
 
-    def _extract_mel_spectrogram(self, loaded_signals):
+        for signal in loaded_signals:
+
+            mfcc = librosa.feature.mfcc(signal,
+                                         sr=self.sample_rate,
+                                          S=None, 
+                                          n_fft= self.n_fft,
+                                          n_mfcc= self.n_mfcc)
+
+            featrue_list.append(mfcc)                              
+
+        return featrue_list
+
+
+     def extract_mel_spectrogram(self, loaded_signals):
     
         """Function to extract mel spectrograms from loaded audio files using librosa.
             
@@ -79,20 +123,78 @@ class Pipeline:
                 loaded_signals (float): list of signals for each audio file
             
             Returns: 
-                features (numpy arrays): list of mel spectrograms extracted from audio signal
+                features ( arrays list): list of mel spectrograms extracted from audio signal
         
         """
 
-    def _save_features(self): 
+        featrue_list=[]
 
-      """Function to save extracted features from audio signals.
-		 filenames to use : "originalfilename_" + feature type + ".npy"
-		Args: 
-			None
-		
-		Returns: 
-			None
-	 """        
+        for signal in loaded_signals:
+    
+            sftt = librosa.stft(signal, center=False, n_fft=self.n_fft, hop_length=self.hop_length)
+            S = np.abs(sftt) ** 2
+            S = librosa.feature.melspectrogram(S=S, n_mels=self.mel_bands, sr=self.sample_rate)
+ 
+            featrue_list.append(S)
+
+
+        return featrue_list
+
+
+     def save_features(self, feature, feature_type):
+        
+        """Function to save extracted features from audio signals.
+            filenames to use : "originalfilename_" + feature type + ".npy"
+            Args: 
+                feature_type(string): representing the feature type
+                feature( arrays list): list of extracted feature from audio signal
+            Returns: 
+                None
+        """  
+
+        path, dirs, files = next(os.walk(self.dataset_path))
+
+
+        if feature_type == "melspectrograms":
+
+            
+            for i,f in enumerate(files):
+
+
+                file_path = os.path.join(path, f)
+                original_filename = Path(file_path).stem
+
+                melspectrograms = feature[i]
+                
+                save_filename = f"{original_filename}_{feature_type}"
+
+                save_file_path = os.path.join(self.output_path, save_filename)
+              
+                # save the feature as numpy array
+                np.save(save_file_path,melspectrograms)
+
+        elif feature_type == "mfccs":
+
+
+            
+            for i,f in enumerate(files):
+
+
+                file_path = os.path.join(path, f)
+                original_filename = Path(file_path).stem
+
+                mfcc = feature[i]
+                
+                save_filename = f"{original_filename}_{feature_type}"
+
+                save_file_path = os.path.join(self.output_path, save_filename)
+                # save the feature as numpy array
+
+                np.save(save_file_path,mfcc)
+
+
+        print("extracted features saved successfully in {} direcotry"\
+            .format(self.output_path))
 
 
 
@@ -100,13 +202,11 @@ class Pipeline:
 
 
 def main():
-    if len(sys.argv) == 3:
+    # if len(sys.argv) == 3:
 
-
-    
-    else:
+    # else:
 
 
 
-if __name__ == '__main__':
+ if __name__ == '__main__':
     main()      
--- bin/preprocessor/test_pipeline.py
+++ bin/preprocessor/test_pipeline.py
@@ -1,40 +1,138 @@
+import os
 
-#import pytest
+from preprocessor.Preprocessor import Pipeline
 
+import pytest
+import numpy as np
+import librosa
 
-class TestClass(unittest.TestCase):
-
-
-
-
-
-
-    def test_loader(self):
-        """
-        Test that it can load audio files
-        """
-
-        pass
-
-    def test_mfccs(self):
-        """
-        Test that it can extract mfccs from audio signals
-        """
-        pass
-
-    def test_mel_spectrogram(self):
-        """
-        Test that it can extract mel spectrogram from audio signals
-        """    
-
-        pass
-
-    def test_mel_spectrogram(self):
-        """
-        Test that it can save extracted feature as numpy array
-        """ 
-        pass   
+@pytest.fixture(scope='module')
+def pipeline():
+    print('------------------setup-------------------')
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+    pipeline = Pipeline(dataset_path='music_dataset',
+                        output_path='output',
+                        sample_rate=22050,
+                        mel_bands=10,
+                        n_mfcc=10,
+                        hop_length=512)
+
+    yield pipeline
+    print('------------------teardown-------------------')
+    pipeline.loader.cache_clear()
+
+
+@pytest.fixture(scope='module')
+def loaded_signals(pipeline):
+
+    signal = pipeline.loader()
+
+    return signal
+
+
+def test_loader(pipeline,loaded_signals):
+    """
+    Test that it can load audio files
+
+    """
+ 
+
+    expected_signal = []
+    path, dirs, files = next(os.walk(pipeline["dataset_path"]))
+
+    file_count = len(files)
+
+    for f in files:
+
+        file_path = os.path.join(path, f)
+
+        loaded_audio_signal, _ = librosa.load(file_path, sr=pipeline["sample_rate"])
+        expected_signal.append(loaded_audio_signal)
+
+    actual_signal = loaded_signals
+
+    for i in range(file_count):
+        assert np.allclose(actual_signal[i], expected_signal[i])
+
+
+def test_mfccs(pipeline, loaded_signals):
+    """
+    Test that it can extract mfccs from audio signals
+    """
+ 
+
+    path, dirs, files = next(os.walk(pipeline["dataset_path"]))
+
+    file_count = len(files)
+
+    mfcc = pipeline.extract_mfccs(loaded_signals)
+
+    for i in range(file_count):
+        assert mfcc[i].shape[0] == pipeline["n_mfcc"]
+
+
+def test_mel_spectrogram(pipeline, loaded_signals):
+    """
+    Test that it can extract mel spectrogram from audio signals
+    """
+
+   
+    path, dirs, files = next(os.walk(pipeline["dataset_path"]))
+    file_count = len(files)
+
+    mel_spectorgram = pipeline.extract_mel_spectrogram(loaded_signals)
+
+    for i in range(file_count - 1):
+        assert mel_spectorgram[i].shape[0] == pipeline["mel_bands"]
+
+
+def test_save_mfccs(pipeline, loaded_signals):
+    """
+    Test that it can save extracted save_mfccs as numpy array
+    """
+
+    # now load the features and check if it is loaded correctly
+    path, dirs, files = next(os.walk(pipeline.output_path))
+
+    mfccs = pipeline.extract_mfccs(loaded_signals)
+
+    pipeline.save_features(mfccs, "mfccs")
+
+    # now load the features and check if it is loaded correctly
+    path, dirs, files = next(os.walk(pipeline["output_path"]))
+
+
+
+    for f in files:
+
+        if "mfccs" in str(f):
+
+            file_path = os.path.join(path, f)
+
+            # load array
+            data = np.load(file_path)
+            assert data.shape[0] == pipeline["n_mfcc"]
+
+
+def test_save_mel_spectrograms(pipeline, loaded_signals):
+    """
+    Test that it can save extracted mel_spectrograms as numpy array
+    """
+    mel_spectrogram = pipeline.extract_mel_spectrogram(loaded_signals)
+
+    pipeline.save_features(mel_spectrogram, "melspectrograms")
+
+    # now load the features and check if it is loaded correctly
+    path, dirs, files = next(os.walk(pipeline["output_path"]))
+
+    for f in files:
+
+        if "mel_spectrogram" in str(f):
+
+            file_path = os.path.join(path, f)
+
+            # load array
+            data = np.load(file_path)
+            assert data[i].shape[0] == pipeline["mel_bands"]
